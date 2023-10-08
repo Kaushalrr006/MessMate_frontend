efc45ff19e345646db89a63a3e416d1b6ee15b77
Shubham Dandge <shubham.dandge@Linkcode.in>
2023-10-08 12:23:18 +0530

    Routing Cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardsComponent } from './cards/cards.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: DashboardComponent
+},
+{
+  path: 'dashboard', component: DashboardComponent
+},
+{
+  path: 'navbar', component: NavbarComponent
+},
+{
+  path: 'cards', component: CardsComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
